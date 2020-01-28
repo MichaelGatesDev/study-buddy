36e3a6a6e0c3da1f45a76a946d2503c87b381e58
Michael Gates <michaelgatesdev@gmail.com>
2020-01-28 14:42:45 -0500

    Initial commit

---
+++ packages/common/src/models/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    public name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    public doSomething() { }
+
+    public eat() { }
+    public drink(){}
+    public asdf(){}
+}
+
+
