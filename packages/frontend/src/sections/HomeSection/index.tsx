961267c52d32b182959f98fa2d9d7586aed376d9
JolizbethBonilla <jolizbeth98@gmail.com>
2020-02-20 22:51:13 -0500

    Fixed home page style

--- packages/frontend/src/sections/HomeSection/index.tsx
+++ packages/frontend/src/sections/HomeSection/index.tsx
@@ -2,36 +2,42 @@ import React from 'react'
 import "./style.scss";
     import Button from '../../components/Button';
 
+
 const HomeSection = () => {
     return (
         <section>
             <div className = "HomeSection">
-                <h1>edYOU</h1>
-                <div id = "Container">
-                    <form>
-                        <div id = "Login">
+            <img src={'/../../images/edYou.png'} alt ="" />
+                    <div id = "Container">
+                        <form>
                             <h2>Login</h2>
-                            <p>Username</p>
-                            <input type="text" name="Username" id="usr"/>
-                            <p>Password</p>
-                            <input type="password" name= "Password" id= "PwdLogin"/>
-                            <Button title="Login" value="Login"></Button>
-                        </div>
-                    </form>
-                    <form>
-                        <div id = "SignUp">
-                            <h2>Sign Up</h2>
-                            <p>School Email</p>
-                            <input type="email" name="" id=""/>
-                            <p>Password</p>
-                            <input type="password" name="" id="PwdSignUp"/>
-                            <p>Repeat Password</p>
-                            <input type="password" name="" id="PwdSignUp2"/>
-                            <Button title = "Signup" value ="Sign Up"></Button>
-                        </div>
-                    </form>
-                </div>   
-            </div>
+                            <div className="login-group">
+                                <p>Username</p>
+                                <input type="text" className="login-control" />
+                            </div>
+                            <div className="login-group">
+                                <p>Password</p>
+                                <input type="password" className="login-control"/>
+                            </div>
+                            <Button title="submit" value ="Login"></Button>
+                            <p className="forgot-password text-right">
+                               <a href="#"> Forgot password?</a>
+                            </p>
+                        </form>
+                        <form>
+                            <div className = "SignUp-group">
+                                <h2>Sign Up</h2>
+                                    <p>School Email</p>
+                                    <input type="text" className="signup-control" />
+                                    <p>Password</p>
+                                <input type="password" name="" id="PwdSignUp"/>
+                                <p>Repeat Password</p>
+                                <input type="password" name="" id="PwdSignUp2"/>
+                                <Button title = "Signup" value ="Sign Up"></Button>
+                            </div>
+                        </form>
+                    </div> 
+                </div>
         </section>
     );
 };
