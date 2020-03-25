c4b8dc20c2eff1b0a4ec9480c7e665f4aa8831b1
Michael Gates <michaelgatesdev@gmail.com>
2020-03-24 22:37:06 -0400

    Add cors

--- packages/backend/src/Server.ts
+++ packages/backend/src/Server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { Request, Response } from "express";
 import logger from "morgan";
 import path from "path";
+import cors from "cors";
 
 import BaseRouter from "./routes";
 
@@ -14,6 +15,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
+app.options("*", cors());
 app.use("/", BaseRouter);
 app.use(express.static("public"));
 
