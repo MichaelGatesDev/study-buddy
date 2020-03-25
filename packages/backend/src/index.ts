7bf3fc0af84b886a25b0b0c65d913155b17e2000
Michael Gates <michaelgatesdev@gmail.com>
2020-03-24 23:53:52 -0400

    use our own logger instead of winston

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,13 @@
 import "./LoadEnv"; // Must be the first import
 import app from "./Server";
-import { logger } from "./shared/Logger";
 import { sequelize } from "./db/database";
+import { Logger } from "@study-buddy/common";
 
 (async (): Promise<void> => {
   // Start the server
   const port = Number(process.env.PORT || 3000);
   app.listen(port, () => {
-    logger.info("Express server started on port: " + port);
+    Logger.info("Express server started on port: " + port);
   });
 
   try {
@@ -16,10 +16,10 @@ import { sequelize } from "./db/database";
 
     // Connect ot the database
     await sequelize.authenticate();
-    logger.info("Database connection has been established successfully.");
+    Logger.info("Database connection has been established successfully.");
   } catch (error) {
     // console.error(error);
-    logger.error(
+    Logger.error(
       "Unable to connect to the database! Reason: " + error?.message
     );
   }
