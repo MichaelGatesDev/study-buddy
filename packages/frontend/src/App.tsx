d34fe1ffa50f4026ca9d873c0877a9086f0526d7
Michael Gates <michaelgatesdev@gmail.com>
2020-04-02 16:18:51 -0400

    Use react-bootstrap instead of reactstrap, begin landing rework

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,6 +3,15 @@ import "./App.css";
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
+// -- START BOOTSTRAP --
+import "popper.js/dist/umd/popper.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "jquery/dist/jquery.slim";
+// -- END BOOTSTRAP --
+
 import AuthSection from "./sections/AuthSection";
 import LandingSection from "./sections/Landing";
 import ProfileSection from "./sections/ProfileSection";
