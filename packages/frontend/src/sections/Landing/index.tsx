ee68517168cf3541d8999879d2c1efa45cab2146
JolizbethBonilla <jolizbeth98@gmail.com>
2020-04-14 17:49:40 -0400

    Fixed Gap with the Cards, and Added a background

--- packages/frontend/src/sections/Landing/index.tsx
+++ packages/frontend/src/sections/Landing/index.tsx
@@ -6,22 +6,39 @@ import Button from "../../components/Button";
 const LandingSection = () => {
   return (
     <section id="landing-section">
-      <div className ="landing-section container-fluid">
-        <div className="d-flex ">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <div className="d-flex "> 
+          <img src={"/../../images/logo.png"} id ="logo"/>
           <div className="ml-auto p-2">
-          <Button title =" submit" value ="Login /Sign Up"></Button>
+           
+          <button type="button" className="btn btn-secondary btn-sm"id="button" > 
+            {" "}
+            LOGIN / SIGN UP
+          </button>
           </div>
         </div>
-        <h1>Educate You</h1>
-        <p>Our Slogan would go here</p>
-        <h3>Highlights</h3>
-        <div className= "row align-items-start"> 
+      </nav>
+       
+        <div className= "landingImg d-flex align-content-end flex-wrap"> 
+        
+          <div className= "container-fluid d-flex justify-content-center">
+            <button type="button" className="btn btn-secondary btn-lg" id="learnMore">Learn More</button>
+          </div>  
+        </div>
+
+        <div className ="container-fluid">
+        <h1 className="display-2" >Educate You</h1>
+       
+        <h2> Slogan would go here</h2>
+        <hr></hr>
+        <div className= "row d-flex justify-content-center"> 
         <div className= "row justify-content-start">
           <div className="col-4">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Highlight</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Somethine about how its useful</h6>
+                <h5 className="card-title">Highlight 1</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Something about how its useful</h6>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="card-link">Learn More</a>
                 <a href="#" className="card-link">Another link</a>
@@ -29,39 +46,43 @@ const LandingSection = () => {
           </div>
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Highlight</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Somethine about how its useful</h6>
+              <h5 className="card-title">Highlight 2</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Something about how its useful</h6>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="card-link">Learn More</a>
+                <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+        </div>  
+        </div>
+        <div className= "row justify-content-start">
+        <div className="col-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Highlight 3</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Something about how its useful</h6>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="card-link">Learn More</a>
+                <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Highlight 4</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Something about how its useful</h6>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" className="card-link">Learn More</a>
                 <a href="#" className="card-link">Another link</a>
             </div>
           </div>
         </div>
-        <div className="col-4">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Highlight</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Somethine about how its useful</h6>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="card-link">Learn More</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Highlight</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Somethine about how its useful</h6>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="card-link">Learn More</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
-          </div>
 
-        </div>
-        <div className="col-4">
-        <img src={"/../../images/layout.png"} id="layout" alt="" />
-        </div>
       </div>
+        <div className= "row justify-content-center ">
+        <div className="col-8">
+          <img src={"/../../images/layout.png"} id="layout" alt="" />
+        </div>
+        </div>
       </div>
      
 
