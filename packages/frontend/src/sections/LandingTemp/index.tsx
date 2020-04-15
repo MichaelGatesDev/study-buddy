bfd43f74bc9813e1fc781228bdb24125b92f50fd
Michael Gates <michaelgatesdev@gmail.com>
2020-04-14 22:01:24 -0400

    Remove footer

--- packages/frontend/src/sections/LandingTemp/index.tsx
+++ packages/frontend/src/sections/LandingTemp/index.tsx
@@ -334,19 +334,7 @@ export const LandingTempSection = () => {
             </div>
           </div>
           <hr className="featurette-divider" />
-          {/* /END THE FEATURETTES */}
         </div>
-        {/* /.container */}
-        {/* FOOTER */}
-        <footer className="container">
-          <p className="float-right">
-            <a href="#">Back to top</a>
-          </p>
-          <p>
-            © 2017-2019 Company, Inc. · <a href="#">Privacy</a> ·{" "}
-            <a href="#">Terms</a>
-          </p>
-        </footer>
       </main>
     </section>
   );
