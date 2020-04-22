8f38589c438755c9e820369e51b8ecab635fdd42
JolizbethBonilla <jolizbeth98@gmail.com>
2020-04-22 12:51:57 -0400

    Hightlight Txt

--- packages/frontend/src/sections/Landing/index.tsx
+++ packages/frontend/src/sections/Landing/index.tsx
@@ -5,6 +5,7 @@ import Button from "../../components/Button";
 
 const LandingSection = () => {
   return (
+    
     <section id="landing-section">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         
@@ -37,7 +38,7 @@ const LandingSection = () => {
           <div className="col-4">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Highlight 1</h5>
+                <h5 className="card-title">Anonymous Chatting</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Something about how its useful</h6>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="card-link">Learn More</a>
