dbcf89875bd9d52686035cf910e3587b7fb0b2fa
Michael Gates <michaelgatesdev@gmail.com>
2020-04-25 02:35:32 -0400

    Gracefully generate a configuration file if one does not already exist

--- packages/backend/src/db/database.ts
+++ packages/backend/src/db/database.ts
@@ -1,18 +1,37 @@
 import { Sequelize } from "sequelize-typescript";
+import fs from "fs";
 
-import dbConfig from "../db-config.json";
 import { User } from "./models/user";
 import { School } from "./models/school";
 import { Course } from "./models/course";
 import { CourseUser } from "./models/course-user";
 
-export const sequelize = new Sequelize(
-  dbConfig["database"],
-  dbConfig["username"],
-  dbConfig["password"],
-  {
-    host: dbConfig["host"],
-    dialect: "mysql",
-    models: [School, User, Course, CourseUser],
-  }
-);
+interface DatabaseConfig {
+  host: string;
+  database: string;
+  username: string;
+  password: string;
+}
+
+if (!fs.existsSync("./db-config.json")) {
+  fs.writeFileSync(
+    "./db-config.json",
+    JSON.stringify(
+      {
+        host: "localhost",
+        database: "edyou",
+        username: "root",
+        password: "",
+      },
+      null,
+      4
+    )
+  );
+}
+
+const dbConfig = JSON.parse(fs.readFileSync("./db-config.json", "UTF-8")) as DatabaseConfig;
+export const sequelize = new Sequelize(dbConfig["database"], dbConfig["username"], dbConfig["password"], {
+  host: dbConfig["host"],
+  dialect: "mysql",
+  models: [School, User, Course, CourseUser],
+});
