85b52d555983e9c5023f08d4d309a2e0db131f55
Michael Gates <michaelgatesdev@gmail.com>
2020-05-06 04:14:09 -0400

    Fix dropdown not selecting current school

--- packages/frontend/src/sections/SettingsSection/index.tsx
+++ packages/frontend/src/sections/SettingsSection/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
-import { useHistory, withRouter } from "react-router";
+import { withRouter } from "react-router";
 import { ThunkDispatch } from "redux-thunk";
 import { Action } from "redux";
 
-import { IUser, ISchool } from "@study-buddy/common";
+import { IUser } from "@study-buddy/common";
 
 import { AppState } from "../../redux/store";
 import { AuthState } from "../../redux/auth/types";
@@ -21,7 +21,9 @@ import { updateUser } from "../../redux/users/actions";
 
 interface Props {
   authState: AuthState;
+
   schoolsState: SchoolState;
+
   usersState: UserState;
   updateUser: (
     user: IUser
@@ -29,16 +31,14 @@ interface Props {
 }
 
 const SettingsSection = (props: Props): JSX.Element => {
-  const history = useHistory();
-
   const [validated, setValidated] = useState<boolean>(false);
+  const [selectedSchoolID, setSelectedSchoolID] = useState<number>(-1);
 
   const user = props.authState?.authedUser;
-  const school = user?.school;
 
-  const [settingSchool, setSettingSchool] = useState<number | undefined>(
-    school?.id ?? -1
-  );
+  useEffect(() => {
+    setSelectedSchoolID(user?.school_id ?? -1);
+  }, [user]);
 
   const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     const form = event.currentTarget;
@@ -48,7 +48,7 @@ const SettingsSection = (props: Props): JSX.Element => {
       return;
     }
 
-    if (settingSchool === -1) {
+    if (selectedSchoolID === -1) {
       alert("You must select a school!");
       return;
     }
@@ -58,7 +58,7 @@ const SettingsSection = (props: Props): JSX.Element => {
     props
       .updateUser({
         id: user?.id,
-        school_id: settingSchool,
+        school_id: selectedSchoolID,
       } as IUser)
       .then((payload: UpdateUserSuccessPayload | UpdateUserFailurePayload) => {
         if (payload.type === UPDATE_USER_SUCCESS) {
@@ -66,7 +66,9 @@ const SettingsSection = (props: Props): JSX.Element => {
           // const user = payload.data;
           console.log("Succesfully Updated user!");
           // alert("Settings saved!");
-          history.push("/settings");
+
+          // reload so we can fetch newest
+          window.location.reload();
         } else {
           payload = payload as UpdateUserFailurePayload;
           console.error("Failed to update user!");
@@ -78,8 +80,6 @@ const SettingsSection = (props: Props): JSX.Element => {
         console.error(err);
       });
   };
-
-  // console.log(user);
 
   return (
     <section>
@@ -93,10 +93,10 @@ const SettingsSection = (props: Props): JSX.Element => {
           <div className="col">
             <Form onSubmit={onFormSubmit} noValidate validated={validated}>
               <SchoolsDropdown
-                value={settingSchool} //TODO set to user's current school
+                value={selectedSchoolID} //TODO set to user's current school
                 onChange={(newValue: number): void => {
                   console.log("School dropdown value is: " + newValue);
-                  setSettingSchool(newValue);
+                  setSelectedSchoolID(newValue);
                 }}
               />
               <Button variant="success" type="submit" className="w-100 mb-4">
